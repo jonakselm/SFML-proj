1dd0c351d5cc2447746bfd38cead7c942872c17f
Jon Aksel Møller <jonakselm@gmail.com>
2019-01-22 10:34:38 +0100

    Made apple's position follow the same gridas snake

--- SFML-proj/Apple.cpp
+++ SFML-proj/Apple.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "Apple.h"
 #include "Board.h"
+#include <random>
 
 Apple::Apple() :
 	m_shape(15.f)
@@ -12,7 +13,10 @@ Apple::Apple() :
 
 void Apple::respawn()
 {
-	m_shape.setPosition(float(std::rand() % (Board::dim * (Board::width - 1))), float(std::rand() % Board::dim * (Board::height - 1)));
+	Location newLoc;
+	newLoc.x = float(std::rand() % (Board::width - 1));
+	newLoc.y = float(std::rand() % (Board::height - 1));
+	m_shape.setPosition((newLoc.x * Board::dim), (newLoc.y * Board::dim));
 }
 
 void Apple::draw(sf::RenderTarget &target) const
